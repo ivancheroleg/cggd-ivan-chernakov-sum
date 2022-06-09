79726d0f218baae6cadb8bcc6c0325d87fecec64
Ivan <ivancholeg@gmail.com>
2022-06-09 09:14:30 +0300

    lab01 cg::resource

--- src/resource.h
+++ src/resource.h
@@ -36,10 +36,16 @@ namespace cg
 	template<typename T>
 	inline resource<T>::resource(size_t size)
 	{
+		data.resize(size);
+		stride = 0;
+
 	}
 	template<typename T>
 	inline resource<T>::resource(size_t x_size, size_t y_size)
 	{
+		data.resize(x_size * y_size);
+		stride = x_size;
+
 	}
 	template<typename T>
 	inline resource<T>::~resource()
@@ -48,27 +54,39 @@ namespace cg
 	template<typename T>
 	inline const T* resource<T>::get_data()
 	{
+		return data.data();
+
 	}
 	template<typename T>
 	inline T& resource<T>::item(size_t item)
 	{
+		return data.at(item);
+
 	}
 	template<typename T>
 	inline T& resource<T>::item(size_t x, size_t y)
 	{
+		return data.at(y*stride + x);
+
 	}
 	template<typename T>
 	inline size_t resource<T>::get_size_in_bytes() const
 	{
+		return data.size()*item_size;
+
 	}
 	template<typename T>
 	inline size_t resource<T>::get_number_of_elements() const
 	{
+		return data.size();
+
 	}
 
 	template<typename T>
 	inline size_t resource<T>::get_stride() const
 	{
+		return stride;
+
 	}
 
 	// TODO: Lab 1.02. Implement `cg::color` and `cg::unsigned_color` structs
@@ -76,9 +94,14 @@ namespace cg
 	{
 		static color from_float3(const float3& in)
 		{
+			color color{in.x, in.y, in.z};
+			return color;
+
 		};
 		float3 to_float3() const
 		{
+			return float3{r,g,b};
+
 		}
 		float r;
 		float g;
@@ -89,13 +112,29 @@ namespace cg
 	{
 		static unsigned_color from_color(const color& color)
 		{
+			unsigned_color out{};
+			out.r = std::clamp(static_cast<int>(255.f * color.r), 0, 255);
+			out.g = std::clamp(static_cast<int>(255.f * color.g), 0, 255);
+			out.b = std::clamp(static_cast<int>(255.f * color.b), 0, 255);
+
 		};
 		static unsigned_color from_float3(const float3& color)
 		{
+			unsigned_color out{};
+			float3 preprocessed = clamp(255.f * color, 0 , 255);
+			out.r = static_cast<int>(preprocessed.x);
+			out.g = static_cast<int>(preprocessed.y);
+			out.b = static_cast<int>(preprocessed.z);
+			return out;
 
 		};
 		float3 to_float3() const
 		{
+						return float3 {
+								static_cast<float>(r),
+								static_cast<float>(g),
+								static_cast<float>(b),
+						}	/	255.f;
 
 		};
 		unsigned char r;
