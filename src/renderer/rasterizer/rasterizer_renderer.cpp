238bfdd3b2e84926322359243712adbc63fcf4a4
Ivan <ivancholeg@gmail.com>
2022-06-09 09:29:51 +0300

    Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -12,8 +12,28 @@ void cg::renderer::rasterization_renderer::init()
 			cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
 	rasterizer->set_render_target(render_target);
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+
+	model = std::make_shared<cg::world::model>();
+	model ->load_obj(settings -> model_path);
 	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
+
+	camera = std::make_shared<cg::world::camera>();
+	camera->set_height(static_cast<float>(settings->height));
+	camera->set_width(static_cast<float>(settings->width));
+	camera->set_position(
+			float3{
+					settings->camera_position[0],
+					settings->camera_position[1],
+					settings->camera_position[2]
+			}
+	);
+	camera->set_phi(settings->camera_phi);
+	camera->set_theta(settings->camera_theta);
+	camera->set_angle_of_view(settings->camera_angle_of_view);
+	camera->set_z_near(settings->camera_z_near);
+	camera->set_z_far(settings->camera_z_far);
+
+
 	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
 	
 }
@@ -22,9 +42,16 @@ void cg::renderer::rasterization_renderer::render()
 	rasterizer->clear_render_target({56,155,90});
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	for(size_t shape_id = 0; shape_id < model->get_index_buffers().size(); shape_id++) {
+		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
+		rasterizer->draw(model->get_vertex_buffers()[shape_id]->get_number_of_elements(), 0);
+	}
+
 	// TODO: Lab 1.04. Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
+
+	cg::utils::save_resource(*render_target, settings->result_path);
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}
