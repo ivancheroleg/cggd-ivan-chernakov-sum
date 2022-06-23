4dd09812451a9e90ae9fddd380173acb4a6a9de7
Ivan <ivancholeg@gmail.com>
2022-06-23 17:53:26 +0300

    3.02. Create a device object

--- src/world/camera.cpp
+++ src/world/camera.cpp
@@ -83,6 +83,7 @@ const float4x4 cg::world::camera::get_view_matrix() const
 #ifdef DX12
 const DirectX::XMMATRIX cg::world::camera::get_dxm_view_matrix() const
 {
+	return DirectX::
 	// TODO Lab 3.08. Implement `get_dxm_view_matrix`, `get_dxm_projection_matrix`, and `get_dxm_mvp_matrix` methods of `camera`
 }
 
@@ -93,6 +94,7 @@ const DirectX::XMMATRIX cg::world::camera::get_dxm_projection_matrix() const
 
 const DirectX::XMMATRIX camera::get_dxm_mvp_matrix() const
 {
+	return DirectX::XMMatrixIdentity();
 	// TODO Lab 3.08. Implement `get_dxm_view_matrix`, `get_dxm_projection_matrix`, and `get_dxm_mvp_matrix` methods of `camera`
 }
 #endif
